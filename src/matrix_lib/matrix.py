2fecd69ef9d87844c2482d65ac06103047ea7355
Dmitry-Malov <113904744+Dmitry-Malov@users.noreply.github.com>
2025-05-07 00:03:44 +0300

    Update Sym Matrix and add Triangle Matrix

--- src/matrix_lib/matrix.py
+++ src/matrix_lib/matrix.py
@@ -1,7 +1,8 @@
 import numpy as np
-from scipy.linalg import lu, inv, qr
+from scipy.linalg import blas, lu, qr, solve_triangular
 
-from .text_block import *
+from .text_block import TextBlock
+
 
 class Matrix:
     """Общий предок для всех матриц."""
@@ -24,19 +25,33 @@ class Matrix:
 
     def __repr__(self):
         """Возвращает текстовое представление для матрицы."""
-        text = [[TextBlock.from_str(f"{self[r, c]}") for c in range(self.width)] for r in range(self.height)]
-        width_el = np.array(list(map(lambda row: list(map(lambda el: el.width, row)), text)))
-        height_el = np.array(list(map(lambda row: list(map(lambda el: el.height, row)), text)))
+        text = [
+            [TextBlock.from_str(f"{self[r, c]}") for c in range(self.width)]
+            for r in range(self.height)
+        ]
+        width_el = np.array(
+            list(map(lambda row: list(map(lambda el: el.width, row)), text))
+        )
+        height_el = np.array(
+            list(map(lambda row: list(map(lambda el: el.height, row)), text))
+        )
         width_column = np.max(width_el, axis=0)
         width_total = np.sum(width_column)
         height_row = np.max(height_el, axis=1)
         result = []
         for r in range(self.height):
             lines = TextBlock.merge(
-                text[r][c].format(width=width_column[c], height=height_row[r]) for c in range(self.width))
-            for l in lines:
-                result.append(f"| {l} |")
-            if len(lines) > 0 and len(lines[0]) > 0 and lines[0][0] == '|' and r < self.height - 1:
+                text[r][c].format(width=width_column[c], height=height_row[r])
+                for c in range(self.width)
+            )
+            for t in lines:
+                result.append(f"| {t} |")
+            if (
+                len(lines) > 0
+                and len(lines[0]) > 0
+                and lines[0][0] == "|"
+                and r < self.height - 1
+            ):
                 result.append(f'| {" " * (width_total + self.width)}|')
         return "\n".join(result)
 
@@ -51,7 +66,9 @@ class Matrix:
 
     def __add__(self, other):
         if isinstance(other, Matrix):
-            assert self.width == other.width and self.height == other.height, f"Shapes does not match: {self.shape} != {other.shape}"
+            assert (
+                self.width == other.width and self.height == other.height
+            ), f"Shapes does not match: {self.shape} != {other.shape}"
             matrix = self.empty_like()
             for r in range(self.height):
                 for c in range(self.width):
@@ -61,7 +78,9 @@ class Matrix:
 
     def __sub__(self, other):
         if isinstance(other, Matrix):
-            assert self.width == other.width and self.height == other.height, f"Shapes does not match: {self.shape} != {other.shape}"
+            assert (
+                self.width == other.width and self.height == other.height
+            ), f"Shapes does not match: {self.shape} != {other.shape}"
             matrix = self.empty_like()
             for r in range(self.height):
                 for c in range(self.width):
@@ -74,7 +93,9 @@ class Matrix:
 
     def __matmul__(self, other):
         if isinstance(other, Matrix):
-            assert self.width == other.height, f"Shapes does not match: {self.shape} != {other.shape}"
+            assert (
+                self.width == other.height
+            ), f"Shapes does not match: {self.shape} != {other.shape}"
             matrix = self.empty_like()
             for r in range(self.height):
                 for c in range(other.width):
@@ -92,8 +113,8 @@ class Matrix:
     def invert_element(self, element):
         if isinstance(element, float):
             return 1 / element
-        if isinstance(element, Fraction):
-            return 1 / element
+        # if isinstance(element, Fraction):
+        #     return 1 / element
         if isinstance(element, Matrix):
             return element.inverse()
         raise TypeError
@@ -151,14 +172,17 @@ class SymmetricMatrix(Matrix):
     def __init__(self, data):
 
         self._size = data.shape[0]
-        self._values = []
+        self._values = np.empty(int(self._size * (self._size + 1) * 0.5))
 
         # Упаковываем только нижний треугольник
+        j = 0
         for i in range(self._size):
-            self._values.append(data[i, :i + 1])
+            self._values[j : j + i + 1] = data[i, : i + 1]
+            j += i + 1
 
         self._lu_cache = None
         self._qr_cache = None
+        self._ldlt_cache = None
 
     @property
     def shape(self):
@@ -166,15 +190,19 @@ class SymmetricMatrix(Matrix):
 
     @property
     def dtype(self):
-        return self._values[0][0].dtype
+        return self._values[0].dtype
 
     def __getitem__(self, key):
         row, col = key
-        return self._values[max(row, col)][min(row, col)]
+        return self._values[
+            int(max(row, col) * (max(row, col) + 1) * 0.5 + min(row, col))
+        ]
 
     def __setitem__(self, key, value):
         row, col = key
-        self._values[max(row, col)][min(row, col)] = value
+        self._values[int(max(row, col) * (max(row, col) + 1) * 0.5 + min(row, col))] = (
+            value
+        )
 
     def empty_like(self, width=None, height=None):
         dtype = self.dtype
@@ -187,75 +215,48 @@ class SymmetricMatrix(Matrix):
 
     def to_dense(self):
         mat = np.zeros((self._size, self._size))
+        j = 0
         for i in range(self._size):
-            mat[i, :i + 1] = self._values[i]
-            mat[:i, i] = self._values[i][:-1]
+            mat[i, : i + 1] = self._values[j : j + i + 1]
+            j += i + 1
+        mat += np.transpose(mat) - mat * np.eye(self._size)
         return mat
 
     @classmethod
-    def zeros(cls, size, dtype=np.float64):
-        new_matrix = cls.__new__(cls)
-        new_matrix._size = size
-        new_matrix._values = [np.zeros(i + 1, dtype=dtype) for i in range(size)]
-        return new_matrix
+    def zeros(cls, size, default=0):
+        new_matrix = np.empty((size, size), dtype=type(default))
+        new_matrix[:] = default
+        return SymmetricMatrix(new_matrix)
 
     def __add__(self, other):
         if isinstance(other, SymmetricMatrix):
             assert self._size == other._size, "Размеры матриц не совпадают"
         result = SymmetricMatrix(np.zeros(self.shape))
-        for i in range(self._size):
-            result._values[i] = self._values[i] + other._values[i]
+        result._values += self._values + other._values
         return result
 
     def __sub__(self, other):
         if isinstance(other, SymmetricMatrix):
             assert self._size == other._size, "Размеры матриц не совпадают"
         result = SymmetricMatrix(np.zeros(self.shape))
-        for i in range(self._size):
-            result._values[i] = self._values[i] - other._values[i]
+        result._values += self._values - other._values
         return result
 
     def __mul__(self, scalar):
-        result = SymmetricMatrix.zeros(self._size, dtype=self.dtype)
-        for i in range(self._size):
-            result._values[i] = self._values[i] * scalar
+        result = SymmetricMatrix.zeros(self._size)
+        result._values += self._values * scalar
         return result
 
     def __matmul__(self, other):
         if isinstance(other, SymmetricMatrix):
-            result = SymmetricMatrix.zeros(self._size)
-            for i in range(self._size):
-                for j in range(i, self._size):
-                    total = 0.0
-                    for k in range(self._size):
-                        a_ik = self._values[max(i, k)][min(i, k)] if k != i else self._values[i][i]
-                        b_kj = other._values[max(k, j)][min(k, j)] if k != j else other._values[j][j]
-                        total += a_ik * b_kj
-                    result._values[j][i] = total
-            return result
+            return blas.dsymm(1.0, self.to_dense(), other.to_dense())
         else:
             return self.to_dense() @ other
 
-    def lu_decomposition(self):
+    def plu_decomposition(self):
 
         if self._lu_cache is not None:
             return self._lu_cache
-        # TODO: можно как то использоваьб LDL^T разложение для полож. опр. матриц
-
-        #     n = self._size
-        #     L = np.eye(n)
-        #     D = np.zeros(n)
-        #
-        #     for j in range(n):
-        #         D[j] = self._values[j][j] - sum(L[j,k]**2 * D[k] for k in range(j))
-        #
-        #         for i in range(j+1, n):
-        #             L[i,j] = (self._values[i][j] - sum(L[i,k]*L[j,k]*D[k] for k in range(j))) / D[j]
-        #
-        #     ldlt = True
-        #     self._lu_cache = (L, D, ldlt)
-        #     return L, D, ldlt
-
         P, L, U = lu(self.to_dense())
         self._lu_cache = (P, L, U)
         return P, L, U
@@ -267,18 +268,303 @@ class SymmetricMatrix(Matrix):
         self._qr_cache = (Q, R)
         return Q, R
 
-    def det(self):
+    def ldlt_decomposition(self):
+        if self._ldlt_cache is not None:
+            return self._ldlt_cache
 
-        _, R = self.qr_decomposition()
-        det = R.diagonal().prod()
-        return det
+        L = np.eye(self._size)  # Нижнетреугольная с единичной диагональю
+        D = np.zeros(self._size)  # Диагональ
+
+        for j in range(self._size):
+            D[j] = self[j, j] - np.sum(L[j, :j] ** 2 * D[:j])
+
+            # Если D[j,j] = 0 → разложение невозможно
+            if np.isclose(D[j], 0):
+                raise ValueError("LDLᵀ невозможно: нулевой элемент на диагонали D")
+
+            # Вычисляем L[i,j] для i > j
+            for i in range(j + 1, self._size):
+                L[i, j] = (self[i, j] - np.sum(L[i, :j] * L[j, :j] * D[:j])) / D[j]
+        D = np.diag(D)
+        self._ldlt_cache = (L, D)
+        return L, D
+
+    def det(self):
+        if self._ldlt_cache is not None:
+            return self._ldlt_cache[1].diagonal().prod()
+        if self._qr_cache is not None:
+            return self._qr_cache[1].diagonal().prod()
+
+        try:
+            _, D = self.ldlt_decomposition()
+            det = D.diagonal().prod()
+            return det
+        except ValueError:
+            _, R = self.qr_decomposition()
+            det = R.diagonal().prod()
+            return det
 
     def inverse(self):
 
         if np.issubdtype(self.dtype, np.number):
-            inv_data = inv(self.to_dense())
-            return SymmetricMatrix(inv_data)
+            if self._ldlt_cache is not None:
+                L, D = self._ldlt_cache
+            else:
+                L, D = self.ldlt_decomposition()
+
+            inv_L = solve_triangular(L, np.eye(self._size, lower=True))
+            inv_D = np.diag(1 / np.diag(D))
+            return inv_L.T @ inv_D @ inv_L
+
         raise NotImplementedError("Инверсия реализована только для числовых матриц")
+
+    def solve_slau(self, b):
+        try:
+            if self._ldlt_cache is not None:
+                L, D = self._ldlt_cache
+            else:
+                L, D = self.ldlt_decomposition()
+
+            y = solve_triangular(L, b, lower=True)  # Ly = b
+            z = y / np.diag(D)  # Dz = y
+            x = solve_triangular(L.T, z, lower=False)  # Lᵀx = z
+            return x
+
+        except ValueError:
+            return np.linalg.solve(self.to_dense(), b)
+
+    def __repr__(self):
+        # Используем родительский __repr__
+        return super().__repr__()
+
+
+class TriangularMatrix(Matrix):
+    """Инициализируется 2 параметра: 1) сама матрица data, 2) параметр transp = False если матрица нижнетреугольная
+    и равен True если матрица верхнетреугольная."""
+
+    def __init__(self, data, transp=False):
+
+        self._size = data.shape[0]
+        self._values = np.empty(int(self._size * (self._size + 1) * 0.5))
+        self._transp = transp
+        if not transp:  # Упаковываем только нижний треугольник
+            j = 0
+            for i in range(self._size):
+                self._values[j : j + i + 1] = data[i, : i + 1]
+                j += i + 1
+
+        else:  # транспонированием переводим в нижнетреугольную матрицу и устанавливаем флаг в self._transp
+            data = np.transpose(data)
+            j = 0
+            for i in range(self._size):
+                self._values[j : j + i + 1] = data[i, : i + 1]
+                j += i + 1
+            self._transp = True
+
+        self._lu_cache = None
+        self._qr_cache = None
+
+    @property
+    def shape(self):
+        return (self._size, self._size)
+
+    @property
+    def dtype(self):
+        return self._values[0].dtype
+
+    def __getitem__(self, key):
+        row, col = key
+        if (not self._transp and col > row) or (self._transp and col < row):
+            return 0
+        return self._values[
+            int(max(row, col) * (max(row, col) + 1) * 0.5 + min(row, col))
+        ]
+
+    def __setitem__(self, key, value):
+        row, col = key
+        assert not (
+            (not self._transp and col > row) or (self._transp and col < row)
+        ), "Матрица выйдет из класса треугольных"
+        self._values[int(max(row, col) * (max(row, col) + 1) * 0.5 + min(row, col))] = (
+            value
+        )
+
+    def empty_like(self, width=None, height=None):
+        dtype = self.dtype
+        if width is None:
+            width = self._size
+        if height is None:
+            height = self._size
+        data = np.empty((height, width), dtype=dtype)
+        return TriangularMatrix(data, self._transp)
+
+    def to_dense(self):
+        mat = np.zeros((self._size, self._size))
+        j = 0
+        for i in range(self._size):
+            mat[i, : i + 1] = self._values[j : j + i + 1]
+            j += i + 1
+        if self._transp:
+            mat = np.transpose(mat)
+        return mat
+
+    @classmethod
+    def zeros(cls, size, default=0):
+        new_matrix = np.empty((size, size), dtype=type(default))
+        new_matrix[:] = default
+        return TriangularMatrix(new_matrix)
+
+    def __add__(self, other):
+        if isinstance(other, TriangularMatrix):
+            assert self._size == other._size, "Размеры матриц не совпадают"
+        if self._transp == other._transp:
+            result = TriangularMatrix(np.zeros(self.shape), self._transp)
+            result._values += self._values + other._values
+            return result
+        else:
+            return self.to_dense() + other.to_dense()
+
+    def __sub__(self, other):
+        if isinstance(other, TriangularMatrix):
+            assert self._size == other._size, "Размеры матриц не совпадают"
+        if self._transp == other._transp:
+            result = TriangularMatrix(np.zeros(self.shape), self._transp)
+            result._values += self._values - other._values
+            return result
+        else:
+            return self.to_dense() - other.to_dense()
+
+    def __mul__(self, scalar):
+        result = TriangularMatrix.zeros(self._size)
+        result._values += self._values * scalar
+        result._transp = self._transp
+        return result
+
+    def __matmul__(self, other):
+        if isinstance(other, TriangularMatrix):
+            if (not self._transp) and (not other._transp):
+                return TriangularMatrix(
+                    blas.dtrmm(
+                        1.0,
+                        self.to_dense(),
+                        other.to_dense(),
+                        side=0,
+                        lower=True,
+                        trans_a=0,
+                    ),
+                    False,
+                )
+            elif self._transp and other._transp:
+                return TriangularMatrix(
+                    blas.dtrmm(
+                        1.0,
+                        self.to_dense(),
+                        other.to_dense(),
+                        side=0,
+                        lower=False,
+                        trans_a=0,
+                    ),
+                    True,
+                )
+            elif (not self._transp) and other._transp:
+                return blas.dtrmm(
+                    1.0,
+                    self.to_dense(),
+                    other.to_dense(),
+                    side=0,
+                    lower=True,
+                    trans_a=0,
+                    overwrite_b=False,
+                )
+            else:
+                return blas.dtrmm(
+                    1.0,
+                    self.to_dense(),
+                    other.to_dense(),
+                    side=0,
+                    lower=False,
+                    trans_a=0,
+                    overwrite_b=False,
+                )
+        else:
+            return self.to_dense() @ other
+
+    def det(self):
+        return np.prod(
+            [self._values[int(i * (i + 1) * 0.5 - 1)] for i in range(self._size)]
+        )
+
+    def inverse(self):
+        if not self._transp:
+
+            n = self._size
+            inv_L = np.zeros((self._size, self._size))
+
+            for i in range(n):
+                inv_L[i, i] = 1 / self[i, i]
+                for j in range(i):
+                    inv_L[i, j] = (
+                        -np.sum(
+                            np.fromiter(
+                                (self[i, k] * inv_L[k, j] for k in range(j, i)),
+                                dtype=np.float64,
+                            )
+                        )
+                        / self[i, i]
+                    )
+
+            return inv_L
+
+        else:
+            n = self._size
+            inv_U = np.zeros((self._size, self._size))
+
+            for i in reversed(range(n)):  # Идём снизу вверх
+                inv_U[i, i] = 1 / self[i, i]  # Диагональ
+
+                for j in range(i + 1, n):
+                    inv_U[i, j] = (
+                        -np.sum(
+                            np.fromiter(
+                                (self[i, k] * inv_U[k, j] for k in range(i + 1, j + 1)),
+                                dtype=np.float64,
+                            )
+                        )
+                        / self[i, i]
+                    )
+
+            return inv_U
+
+    def solve_slau(self, b):
+
+        n = self._size
+        x = np.zeros_like(b, dtype=float)
+        if not self._transp:
+            for i in range(n):
+                x[i] = (
+                    b[i]
+                    - np.sum(
+                        np.fromiter(
+                            (self[i, k] * x[k] for k in range(i)), dtype=np.float64
+                        )
+                    )
+                ) / self[i, i]
+
+            return x
+        else:
+
+            for i in reversed(range(n)):
+                x[i] = (
+                    b[i]
+                    - np.sum(
+                        np.fromiter(
+                            (self[i, k] * x[k] for k in range(i + 1, n)),
+                            dtype=np.float64,
+                        )
+                    )
+                ) / self[i, i]
+
+            return x
 
     def __repr__(self):
         # Используем родительский __repr__
